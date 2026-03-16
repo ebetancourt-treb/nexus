c71b69c28e616d66087bcc312b7bdb0ee5137e53
Erick Betancourt <ebetancourt@drg.mx>
2026-03-15 23:39:43 -0600

    cambios en vite para ajustes

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js'
+            ],
             refresh: true,
+            // Dile a Vite cuál es tu nueva carpeta pública
+            publicDirectory: 'public_html', 
         }),
     ],
-    hmr: {
-            host: '0.0.0.0',   // ip 
-            port: 5173,
-            protocol: 'ws'         // WebSocket para HMR
-        },
+    build: {
+        // Dile a Vite dónde guardar los assets compilados
+        outDir: 'public_html/build', 
+        emptyOutDir: true,
+    }
 });
